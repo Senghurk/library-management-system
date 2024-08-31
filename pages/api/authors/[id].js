a32fd346c2fe593ac2016d337d21ffb319487a97
Senghurk <bin4ryhunt3r@gmail.com>
2024-08-31 23:03:22 +0700

    Update the necessary components and API

--- pages/api/authors/[id].js
+++ pages/api/authors/[id].js
@@ -6,10 +6,18 @@ const authorsPath = path.join(process.cwd(), 'data', 'authors.json');
 export default async function handler(req, res) {
   const { id } = req.query;
 
-  if (req.method === 'PUT') {
-    try {
-      const data = await fs.readFile(authorsPath, 'utf8');
-      let authors = JSON.parse(data);
+  try {
+    const data = await fs.readFile(authorsPath, 'utf8');
+    let authors = JSON.parse(data);
+
+    if (req.method === 'GET') {
+      const author = authors.find(a => a.id === id);
+      if (author) {
+        res.status(200).json(author);
+      } else {
+        res.status(404).json({ message: 'Author not found' });
+      }
+    } else if (req.method === 'PUT') {
       const index = authors.findIndex(a => a.id === id);
       if (index !== -1) {
         authors[index] = { ...authors[index], ...req.body, id };
@@ -18,21 +26,15 @@ export default async function handler(req, res) {
       } else {
         res.status(404).json({ message: 'Author not found' });
       }
-    } catch (error) {
-      res.status(500).json({ message: 'Server error', error: error.message });
-    }
-  } else if (req.method === 'DELETE') {
-    try {
-      const data = await fs.readFile(authorsPath, 'utf8');
-      let authors = JSON.parse(data);
-      const newAuthors = authors.filter(a => a.id !== id);
-      await fs.writeFile(authorsPath, JSON.stringify(newAuthors, null, 2));
+    } else if (req.method === 'DELETE') {
+      authors = authors.filter(a => a.id !== id);
+      await fs.writeFile(authorsPath, JSON.stringify(authors, null, 2));
       res.status(204).end();
-    } catch (error) {
-      res.status(500).json({ message: 'Server error', error: error.message });
+    } else {
+      res.setHeader('Allow', ['GET', 'PUT', 'DELETE']);
+      res.status(405).end(`Method ${req.method} Not Allowed`);
     }
-  } else {
-    res.setHeader('Allow', ['PUT', 'DELETE']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error: error.message });
   }
 }
